28e27793fb555d608ee1944c601369289cf72cb8
Blairnation <tonyblair246@gmail.com>
2024-10-11 09:10:02 +0000

    commit changes for full-stack app

--- base/base/settings.py
+++ base/base/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_8!ka#i5ltw*rsndz$&ut=(r15hwwv@z=$rg4a97u$h6hz#u7_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
